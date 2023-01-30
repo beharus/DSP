e8fe5f4be91b03f3a086838a896967c973b3a819
Asomiddin <asomiddinqarshiyev8@gmail.com>
2023-01-30 16:46:54 +0500

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar/navbar';
 import Form from './components/Form/form';
 import './App.css';
-import Footer from './Footer/footer';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
