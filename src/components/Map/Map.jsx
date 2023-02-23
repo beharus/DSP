d5a51b1c2581294bf921c7044c3160e539cf2b1f
Behruz Eshquvatov <113596209+beharus@users.noreply.github.com>
2023-02-23 09:35:32 +0000

    some

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,10 @@
+
+const Map = () => {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1881.9892496562939!2d67.42318546970574!3d37.5051815124023!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38cab550e12634ed%3A0x937d31dc98856201!2s777Avtomoyka!5e0!3m2!1sru!2s!4v1677142195626!5m2!1sru!2s" width="100%" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    )
+}
+
+export default Map
