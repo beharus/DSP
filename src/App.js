b63f99ca5f2ca17dceacb70bd88412af9012cc3b
Asomiddin1 <120255236+Asomiddin1@users.noreply.github.com>
2023-02-23 16:41:30 +0500

    sdfs

--- src/App.js
+++ src/App.js
@@ -13,17 +13,18 @@ import Main from "./components/Main/Main";
 import Postavshik from "./components/postavshik/postavshik";
 import TkPartner from "./components/Tk_Partner/tkPartner";
 import Contact from "./components/Contact/contact";
+import Logistika from "./components/Logistika/log";
 
 function App() {
   return (
     <>
-      
-      
+      <Navbar />
       <Routes>
         <Route path="/Osp" element={<Osp />} />
         <Route path="/Main" element={<Main/>} />
         <Route path="/" element={<Main/>} />
         <Route path="/Latoflex" element={<Latoflex />} />
+        <Route path="/logistika" element={<Logistika/>} />
         <Route path="/Fanera" element={<Fanera />} />
         <Route path="/Dvp" element={<Dvp />} />
         <Route path="/Dsp" element={<Dsp />} />
--- src/components/Logistika/log.jsx
+++ src/components/Logistika/log.jsx
@@ -20,13 +20,16 @@ function Logistika() {
             том, что получат свой заказ вовремя и без потерь качества.
           </p>
 
-         
+
 
         </div>
-         <div className="col-span-2 lg:w-auto lg:col-span-1 pt-[9%]">
-            <img src={require("./log_img/img2.png")} alt="imgg" />
-          </div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25320.727531790664!2d67.39949034434706!3d37.50577320254916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38cab366bcd1ddcd%3A0x5b4f57700f441687!2sJarqurg&#39;on!5e0!3m2!1sru!2s!4v1676281468492!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div className="col-span-2 lg:w-auto lg:col-span-1 pt-[9%]">
+          <img src={require("./log_img/img2.png")} alt="imgg" />
+        </div>
+      </div>
+
+      <div className="inp">
+       <div className="cbb"></div>
       </div>
 
       <Form />
